397e455608ac54e89018cc1da90a5fba6e4f418e
bodi-cmd <bogdan.barbu.dev@gmail.com>
2020-12-26 18:46:11 +0200

    a

---
+++ js/binary_load_blinkr.js
@@ -0,0 +1,33 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+img1 = new Array();
+
+function preload(i){
+    img1[i] = new Image();
+    var url = "./img/Cadre_blinkr/";
+    if(i<=9) url=url +"000"
+    if(i>=10&&i<=99) url=url +"00"
+    if(i>=100&&i<=999) url=url +"0"
+    url = url + i.toString(10);
+    url = url+".webp";
+    img1[i].src = url;
+    console.log(1);
+    sleep(100);
+}
+function binary_load(l,r){
+    for(i=1;i<=Math.floor(Math.log2(r))+1;i++){
+        console.log("i="+i);
+        for(j=1;j<=Math.pow(2,i);j++){
+            console.log("j="+j)
+            preload(Math.floor(r/Math.pow(2,i)*j));
+            console.log(1)
+        }
+    }
+}
+if (document.images) {
+    preload(1);
+    preload(120);
+    binary_load(1,120);
+}
---
+++ js/mouse_animation.js
@@ -0,0 +1,51 @@
+(function() {
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var eventDoc, doc, body;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+        var Xcoord=(Math.floor((event.pageX/window.innerWidth)*120)+1)
+        console.log(Xcoord+"   "+ event.pageX+"   "+ window.innerWidth)
+        if(Xcoord<=1)Xcoord=1;
+        if(Xcoord>=120)Xcoord=120;
+        var url_blinkr = "./img/Cadre_blinkr/";
+        var url_sportier = "./img/Cadre_sportier/";
+        if(Xcoord<=9){
+          url_blinkr=url_blinkr +"000"
+          url_sportier=url_sportier +"000"
+        }
+        if(Xcoord>=10&&Xcoord<=99){
+          url_blinkr=url_blinkr +"00"
+          url_sportier=url_sportier +"00"
+        }
+        if(Xcoord>=100&&Xcoord<=999){
+          url_blinkr=url_blinkr +"0"
+          url_sportier=url_sportier +"0"
+
+        }
+          url_blinkr = url_blinkr + Xcoord.toString(10);
+          url_blinkr = url_blinkr+".webp";
+          url_sportier = url_sportier + Xcoord.toString(10);
+          url_sportier = url_sportier+".webp";
+        document.getElementById("blinkr").src = url_blinkr;
+        document.getElementById("sportier").src = url_sportier;
+
+        
+    }
+})();
